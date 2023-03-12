787e8a5d2384faec55e38dc6c9d9642849550ccf
CIC4DA <dhruv.jain9100@gmail.com>
2023-03-12 12:30:11 +0530

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,23 @@
 
 import React from "react";
 import {createRoot} from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk';
 
 import App from "./App";
+import reducers from './reducers';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <App/>
+    <Provider store = {store}>
+        <App/>
+    </Provider>    
 );
+
+
+// what provider does is --> it is going to keep track of the store, which is that global state that allow us to access that store from anywhere 
+
